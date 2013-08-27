97931641fc8ed87b10662cec64d0ce3867db8a7d
Nir Yariv <niryariv@gmail.com>
2013-08-27 16:57:02 +0300

    use google geoencoding instead of nominatim

--- geoencode.py
+++ geoencode.py
@@ -7,90 +7,67 @@ import codecs
 OUTPUT_FILE = 'data/all_stations.geojson'
 ERROR_FILE = 'data/errors.txt'
 
-
-def in_israel(lon, lat):
-    check = requests.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&sensor=true" % (lat,lon))
-    j = check.json()
-
-    if not (j or j['status'] == 'OK') :
-        print('Problem with googleapi Url at: %s, %s' % lon, lat)
-        return False
-    for k in j['results']:
-        for i in k['address_components']:
-            try:
-                if i['types'][0]==u'country':
-                    return (i['long_name']=='Israel' or i['long_name']=='Palestine')
-            except IndexError:
-                continue
-        print('The country returned is %s instead of Israel' % i['long_name'])
-    return False
-
-def locate(addr):
-	payload={"q" : addr, "format": "json"} 
-
-	r = requests.get("http://nominatim.openstreetmap.org/search", params=payload)
-
-	print r.url
-	j = r.json()
-
-        if not j or j == None:
-		return False
-       # print(j)
-	lon = float(j[0]['lon'])
-	lat = float(j[0]['lat'])
-        print ('lon:',lon,'lat:', lat,'i:',i) 
-        return(lon, lat)
-
-def reverse_str(s):
-	try:
-		rev = s.split()
-		rev.reverse()
-		return u' '.join(rev)
-	except:
-		return s
-
-json_data = open('milkdrop.json')
+json_data = open('data/milkdrop.json')
 data = json.load(json_data, encoding="utf-8")
 
 geojson = {"type": "FeatureCollection", "features" : []}
-trouble_locs = {}
+
+def locate_with_google(addr):
+	payload={"address" : addr, "sensor": "true"} 
+
+	r = requests.get("http://maps.googleapis.com/maps/api/geocode/json", params=payload)
+
+	print r.url
+	print r.text
+	j = r.json()
+
+	if j['status'] != 'OK': 
+		print "bad status"
+		return False 
+
+	res = j['results'][0]
+	country = res['address_components'].pop()
+
+	if country['long_name'] != "Israel" and country['long_name'] != "Palestine":
+		return False
+
+	return res['geometry']['location']
+
+
+
+# here we go
 
 ctr = 0
 for i in data:
 	item = data[i]
+
 	if item['Yeshuv'] is None: 
 		continue
-	# print i, item
 	
 	addr = item['Yeshuv'] + " " + item['Addr']
 	
-	loc = locate(addr)
-	if loc is False:
-		trouble_locs[i]  ="No street address data for " + addr
-		loc = locate(item['Yeshuv'])
-		if loc is False:
-			trouble_locs[i] = "No data at all"
-			continue
+	loc = locate_with_google(addr)
+	print addr, loc 
 
-	lon, lat = loc
-        if not in_israel(lon, lat):
-            trouble_locs[i] = "Not in israel, something wrong"
-            continue
-            
-	print addr, locate(addr)
+	error = not loc
+
+	if loc is False:
+		loc = {'lng': 0, 'lat': 0}
 
 	geojson['features'].append(
 		{
 	      "type": "Feature",
 	      "geometry": {
 	        "type": "Point",
-	        "coordinates": [ lon, lat ]
+	        "coordinates": [ loc['lng'], loc['lat'] ]
 	      },
 	      "properties": {
+	      	"item_id": i,
 	        "name": item['ThanaName'],
-	        "address": addr,
+	        "address": unicode(addr),
 	        "operator": item['Aharait'],
-   	        "phone" : item['Tel1'],
+   	        "phone" : item['tel1'],
+   	        "error" : error
 	      }
 	    }
 	)
@@ -102,9 +79,3 @@ for i in data:
 f = codecs.open(OUTPUT_FILE, 'w', 'utf-8')
 f.write(json.dumps(geojson, indent=4))
 f.close()
-
-f = codecs.open(ERROR_FILE, 'w', 'utf-8')
-f.write(str(trouble_locs))
-f.close()
-
-	
