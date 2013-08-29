0cac5ed65e5d13590e66c948c3740c54928fb8a2
alonisser <alonisser@gmail.com>
2013-08-30 01:01:20 +0300

    script for making a csv file out of stations.geojson

---
+++ make_csv.py
@@ -0,0 +1,25 @@
+#!/user/bin/python
+# -*- coding = utf-8 -*-
+
+import csv
+import json
+from geoencode import OUTPUT_FILENAME
+
+input_file = "".join([OUTPUT_FILENAME,".geojson"])
+
+with open(input_file) as file:
+
+    data = json.loads(file.read())['features']
+    
+with open('data/milk.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile )
+    writer.writerow(['name', 'address', 'location', 'phone','error', 'operator'])
+    for row in data:
+        writer.writerow(
+            [row['properties']['name'].encode('utf-8'),
+             row['properties']['address'].encode('utf-8'),
+            "%s,%s" % (row['geometry']['coordinates'][1], row['geometry']['coordinates'][0]),
+            row['properties']['phone'],
+            row['properties']['error'],
+            row['properties']['operator'].encode('utf-8'),])
+
