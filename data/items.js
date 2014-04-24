193ec8c6b7a2bafb6c599bb50fd18aaa37250ad0
Nir Yariv <niryariv@gmail.com>
2014-04-24 15:25:58 +0300

    with map

--- data/items.js
+++ data/items.js
@@ -6,93 +6,16 @@ items =
       "geometry": {
         "type": "Point",
         "coordinates": [
-          "35.229157",
-          "31.794915"
+          "35.179436",
+          "31.763183"
         ]
       },
       "type": "Feature",
       "properties": {
-        "city": "ירושלים",
-        "error": "",
-        "name": "שיך ג'ראח",
-        "district": "ירושלים",
-        "address": "מרכז רפואי",
-        "operator": "עיריית ירושלים",
-        "phones": "02-5871923",
-        "subdistrict": "ירושלים"
+        "text": "פרג זיפני",
+        "image": "data/photos/1.JPG"
       }
     },
 
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          35.247427,
-          "31.7801783"
-        ]
-      },
-      "type": "Feature",
-      "properties": {
-        "city": "ירושלים",
-        "error": "",
-        "name": "א- טור",
-        "district": "ירושלים",
-        "address": "סולימאן אל פרסי 13",
-        "operator": "עיריית ירושלים",
-        "phones": "02-6261314",
-        "notes": "שבת 8:00-15:00",
-        "subdistrict": "ירושלים",
-        "days": [
-          "8:00-13:00",
-          "8:00-17:00",
-          "8:00-13:00",
-          "8:00-17:00",
-          "8:00-13:00",
-          "סגור"
-        ]
-      }
-    },
-
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          "35.197572",
-          "31.810772"
-        ]
-      },
-      "type": "Feature",
-      "properties": {
-        "city": "ירושלים",
-        "error": "",
-        "name": "רמות  02",
-        "district": "ירושלים",
-        "address": "ראובן מס 131/25",
-        "operator": "עיריית ירושלים",
-        "phones": "02-5484222",
-        "subdistrict": "ירושלים"
-      }
-    },
-
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          35.1751586,
-          31.7856626
-        ]
-      },
-      "type": "Feature",
-      "properties": {
-        "city": "ירושלים",
-        "error": "",
-        "name": "הר  נוף",
-        "district": "ירושלים",
-        "address": "אגסי 10",
-        "operator": "עיריית ירושלים",
-        "phones": "02-5456253",
-        "subdistrict": "ירושלים"
-      }
-    }
   ]
 }
