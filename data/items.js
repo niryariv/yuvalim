7f654fce788e7e0576e225d6c9b03c6383cf8b70
Nir Yariv <niryariv@gmail.com>
2014-04-27 16:24:57 +0300

    all points

--- data/items.js
+++ data/items.js
@@ -1,21 +1,170 @@
 items = 
 {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          "35.179436",
-          "31.763183"
-        ]
-      },
-      "type": "Feature",
-      "properties": {
-        "text": "פרג זיפני",
-        "image": "data/photos/1.JPG"
-      }
-    },
-
-  ]
+    "features": [
+        {
+            "geometry": {
+                "coordinates": [
+                    31.763183333333334,
+                    35.17943611111111
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "1.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.765297222222223,
+                    35.17867222222222
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "2.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.765325,
+                    35.178638888888884
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "3.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.763952777777778,
+                    35.17867777777778
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "4.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.765327777777777,
+                    35.178525
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "5.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.765422222222224,
+                    35.17861111111111
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "6.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.76595,
+                    35.17887222222222
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "7.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.76600277777778,
+                    35.178974999999994
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "8.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.7662,
+                    35.17903888888889
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "9.JPG"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    31.766219444444445,
+                    35.17915833333333
+                ],
+                "type": "Point"
+            },
+            "id": null,
+            "properties": {
+                "image": "10.JPG"
+            },
+            "type": "Feature"
+        }
+    ],
+    "type": "FeatureCollection"
 }
+
+// {
+//   "type": "FeatureCollection",
+//   "features": [
+
+//     {
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [
+//           "35.179436",
+//           "31.763183"
+//         ]
+//       },
+//       "type": "Feature",
+//       "properties": {
+//         "text": "פרג זיפני",
+//         "image": "data/photos/1.JPG"
+//       }
+//     }
+
+
+
+//   ]
+// }
